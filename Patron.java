8d204ac086e09028674566f430dde63eb0e3a813
Andrea Blanco <a0137220@tec.mx>
2025-04-11 08:58:38 -0600

    Primer commit: Subiendo el sistema de gestión de biblioteca

--- Patron.java
+++ Patron.java
@@ -10,8 +10,11 @@ public class Patron implements Runnable {
     @Override
     public void run() {
         try {
+            // Imprimir que el hilo está ejecutando
+            System.out.println(Thread.currentThread().getName() + " está ejecutando.");
+            
             // Simulando un patrón que pide un libro y luego lo devuelve
-            String bookToBorrow = "Libro" + (int) (Math.random() * 10);  // Un título aleatorio
+            String bookToBorrow = "Libro" + (int) (Math.random() * 4 + 1);  // Un título aleatorio entre Libro1 y Libro4
             if (library.borrowBook(bookToBorrow)) {
                 Thread.sleep((int) (Math.random() * 1000));  // Simula tiempo de lectura
                 library.returnBook(bookToBorrow);
